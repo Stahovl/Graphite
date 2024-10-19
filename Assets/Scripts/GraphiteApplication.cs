6d8cea8d804e386ff4fb8c3615be22b3d05228ae
Stahovl <vtahovs@gmail.com>
2024-10-19 14:45:08 +0500

    Add car models, create prefabs, Create FPS display class

---
+++ Assets/Scripts/GraphiteApplication.cs
@@ -0,0 +1,10 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GraphiteApplication : MonoBehaviour
+{
+    private void Awake()
+    {
+        Application.targetFrameRate = 1380;
+    }
+}
